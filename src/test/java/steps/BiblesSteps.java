3167c665afe6e8178c2162cf4f2691909ccc4668
LL <lexlobov@yandex.ru>
2023-05-15 13:38:49 +0700

    small optimization

--- src/test/java/steps/BiblesSteps.java
+++ src/test/java/steps/BiblesSteps.java
@@ -1,12 +1,9 @@
 package steps;
 
 import clients.BiblesClient;
-import com.google.gson.reflect.TypeToken;
 import models.bibles.Bible;
 import models.bibles.Verse;
 
-import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -15,22 +12,20 @@ public class BiblesSteps extends BaseSteps{
     BiblesClient client = new BiblesClient();
 
     public  void searchedTextIsInTopFiveResults(String token, String  lang, String requestText, Verse expectedVerse){
-        //Type bibles = new TypeToken<ArrayList<Bible>>() {}.getType();
-        List<Bible> bibleObjects = client.getBibles(getToken())
-                .log().all()
+        //Получаем список библий
+        List<Bible> bibleObjects = client.getBibles(token)
+                .log().status().log().headers()
                 .extract().body().jsonPath().getList("", Bible.class);
-
+        //Получаем bibleId для выбранного языка
         String bibleId = bibleObjects.stream()
                 .filter(bible -> bible.getLanguage().equals(lang))
                 .findAny()
                 .get()
                 .getId();
-        Type searchAnswers = new TypeToken<ArrayList<Verse>>() {}.getType();
-        List<Verse> searchResults = client.search(getToken(), bibleId, requestText).extract().as(searchAnswers);
-        List<Verse> topFiveSearchResults = searchResults.stream().limit(5).toList();
+        //Получам первые 5 реузльтатов поиска
+        List<Verse> topFiveSearchResults = client.search(token, bibleId, requestText)
+                .extract().body().jsonPath().getList("", Verse.class).stream().limit(5).toList();
 
         assertThat("Searched text should be in first 5 results", topFiveSearchResults.stream().anyMatch(r->r.equals(expectedVerse)));
-
-
     }
 }
