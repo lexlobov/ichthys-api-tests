d8f6db88039bf9c5c67a8e67b615d8e2741c8531
LL <lexlobov@yandex.ru>
2023-05-18 12:42:12 +0700

    added allure, resolved some conflicts

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,17 +1,20 @@
 package tests;
 
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInstance;
 import steps.BaseSteps;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BaseTest {
 
-    protected String email;
-    protected String password;
+    protected  String email;
+    protected  String password;
 
     protected BaseSteps baseSteps = new BaseSteps();
 
@@ -27,21 +30,17 @@ public class BaseTest {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @BeforeAll
-    protected void setUp(){
+    @BeforeEach
+    public void setUp(){
         Properties props = new Properties();
         try (FileInputStream fis = new FileInputStream("local.properties")){
             props.load(fis);
-            setEmail(props.getProperty("adminLogin"));
-            setPassword(props.getProperty("adminPassword"));
+            email = props.getProperty("adminLogin");
+            password = props.getProperty("adminPassword");
         } catch (IOException e) {
             e.printStackTrace();
         }
         baseSteps.getTokenFromAuth(email, password);
-
+        //RestAssured.filters(new AllureRestAssured());
     }
 }
